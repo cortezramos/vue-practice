22c1439b3f91e7efe57ba3462f30daaef4840501
Erick Cortez <cortezramos@hotmail.com>
2025-11-04 12:26:20 -0600

    feat: add LifeCycleComponent with mounted and unmounted lifecycle hooks

---
+++ src/components/LifeCycleComponent.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+    import { onMounted, onUnmounted } from 'vue';
+
+    onMounted(() => {
+        console.log("se monto, llamo a la api")
+    })
+
+    onUnmounted(() => {
+        console.log("de desmonto")
+    })
+
+</script>
+
+<template>
+    <h1>Life Cycle</h1>
+</template>
